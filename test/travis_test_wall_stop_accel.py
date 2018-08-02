120d2e6dab26c27dc76c41ee1ce3c7d3a95620ac
hakkentanoshii <hakkentanoshii@yahoo.co.jp>
2018-08-03 08:36:54 +0900

    Add a travis test

--- test/travis_test_wall_stop_accel.py
+++ test/travis_test_wall_stop_accel.py
@@ -6,20 +6,34 @@ from std_msgs.msg import UInt16
 from std_srvs.srv import Trigger, TriggerResponse
 
 class WallStopAccelTest(unittest.TestCase):
-    def test_node_exist(self):
-        nodes = rosnode.get_node_names()
-        self.assertIn('/wall_stop',nodes, "node does not exist")
-
-    def set_sensor_values(self,lf,ls,rs,rf):
+    def set_and_get(self,lf,ls,rs,rf):
         with open("/dev/rtlightsensor0","w") as f:
             f.write("%d %d %d %d\n" % (rf,rs,ls,lf))
 
-    def get_freqs(self):
+        time.sleep(0.3)
+
         with open("/dev/rtmotor_raw_l0","r") as lf,\
              open("/dev/rtmotor_raw_r0","r") as rf:
             left = int(lf.readline().rstrip())
             right = int(rf.readline().rstrip())
+
         return left,right
+
+
+    def test_node_exist(self):
+        nodes = rosnode.get_node_names()
+        self.assertIn('/wall_stop',nodes, "node does not exist")
+
+#    def set_sensor_values(self,lf,ls,rs,rf):
+#       with open("/dev/rtlightsensor0","w") as f:
+#            f.write("%d %d %d %d\n" % (rf,rs,ls,lf))
+#
+#    def get_freqs(self):
+#       with open("/dev/rtmotor_raw_l0","r") as lf,\
+#             open("/dev/rtmotor_raw_r0","r") as rf:
+#            left = int(lf.readline().rstrip())
+#            right = int(rf.readline().rstrip())
+#        return left,right
 
     def test_io(self):
         #self.set_sensor_values(400,100,100,0)
